d2228369f3ba0af3ffbac39932ebf332a691d6b8
David Novicki <supercycle91@gmail.com>
2018-09-11 13:48:21 -0700

    Update to node v8

--- serverless-workshop/handler.js
+++ serverless-workshop/handler.js
@@ -1,24 +1,16 @@
-'use strict';
-const AWS = require('aws-sdk');
-const dynamo = new AWS.DynamoDB.DocumentClient();
+const AWS = require("aws-sdk");
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-module.exports.saveEmail = (event, context, callback) => {
-	const response = {
-		statusCode: 200,
-		body: JSON.stringify({
-			message: 'Thanks for sending me: ' + event.pathParameters.email
-		})
-	};
-	if (event && event.pathParameters.email) {
-		const params = {
-			TableName: process.env.DYNAMODB_TABLE,
-			Item: {
-				email: event.pathParameters.email
-			}
-		};
-		dynamo.put(params, function(error) {
-			if (error) callback(error);
-			else callback(null, response);
-		});
-	} else callback('No email was provided.');
+module.exports.saveEmail = async event => {
+  if (!event || !event.pathParameters || !event.pathParameters.email)
+    return new Error("No email was provided.");
+  else {
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE,
+      Item: {
+        email: event.pathParameters.email
+      }
+    };
+    return dynamoDb.put(params).promise();
+  }
 };
